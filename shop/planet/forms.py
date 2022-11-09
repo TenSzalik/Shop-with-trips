c06bfe6002e04d2d2d773fb067fb845468155307
Gracjan P. W. Chudziak <gracjanchudziak@gmail.com>
2022-11-09 03:31:28 +0100

    planet: Add register form

---
+++ shop/planet/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
+from user_profile.models import User
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
